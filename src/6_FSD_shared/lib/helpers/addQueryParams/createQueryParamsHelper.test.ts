e9601f31b309b68268fda3cf395a6f16d89c17ef
DND-dev <dev.work@dnd-projects.ru>
2025-02-08 02:52:22 +0300

    fixed test

--- src/6_FSD_shared/lib/helpers/addQueryParams/createQueryParamsHelper.test.ts
+++ src/6_FSD_shared/lib/helpers/addQueryParams/createQueryParamsHelper.test.ts
@@ -5,12 +5,12 @@ describe("createQueryParamsHelperTest", () => {
 	test("1 param", () => {
 		const result = createQueryParams({ expand: "etg" })
 
-		expect(result).toBe("?expand=etg")
+		expect(result).toBe("#/?expand=etg")
 	})
 
 	test("2 param", () => {
 		const result = createQueryParams({ expand: "etg", name: "Luc" })
 
-		expect(result).toBe("?expand=etg&name=Luc")
+		expect(result).toBe("#/?expand=etg&name=Luc")
 	})
 })
