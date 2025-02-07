242f63ff8d6deb01f4a1f509e10ed396e607d3fd
DND-dev <dev.work@dnd-projects.ru>
2025-02-08 02:00:13 +0300

    fixed test unit

--- src/6_FSD_shared/lib/helpers/convertSecondsToTime/convertSecondsToTimeHelper.test.ts
+++ src/6_FSD_shared/lib/helpers/convertSecondsToTime/convertSecondsToTimeHelper.test.ts
@@ -17,8 +17,4 @@ describe("convertSecondsToTimeHelperTest", () => {
 	test("0:59", () => {
 		expect(convertSecondsToTime(3540)).toBe("00:59")
 	})
-
-	test("unix", () => {
-		expect(convertSecondsToTime(1672949001, true)).toBe("23:03")
-	})
 })
